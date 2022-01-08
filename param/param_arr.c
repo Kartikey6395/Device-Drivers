f9db0bf53f86e1ca55f28cfc407c41768638a291
Kartikey6395 <kartikeytripathi6395@gmial.com>
2022-01-08 11:20:10 +0530

    command line arg-string,arrays

--- param/param_arr.c
+++ param/param_arr.c
@@ -1,20 +1,28 @@
-//command line arguments/parameters 
+//command line arguments/parameters as arrays
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/kernel.h>
 #include <linux/moduleparam.h>
 
-static char* charvar = "module_param";
-static int var = 100;
+static char charvar[20];
+static int var[5];
+static int count,i,sum=0;
+static char str;
 
-module_param(charvar,charp,S_IRUGO);
-module_param(var,int,S_IRUGO);
+module_param_string(charvar,charvar,20,S_IRUSR);
+
+module_param_array(var,int,&count,S_IRUSR);
 
 static int __init init_param(void)
 {
     printk(KERN_ALERT "inside init function \n");
     printk(KERN_ALERT "print value of charvar %s \n",charvar);
-    printk(KERN_ALERT "variable= %d \n",var);
+    for(i=0;i<5;i++)
+    {
+    printk(KERN_ALERT "variable = %d ",var[i]);
+    sum += var[i];
+    }
+    printk(KERN_ALERT "sum = %d\n",sum);
     
     return 0;
 }
